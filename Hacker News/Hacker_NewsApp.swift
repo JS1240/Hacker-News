62a42dc86a2329314a177594b5e0e767e6ec8623
Jure Sunic <juresunic@hotmail.com>
2022-06-03 11:19:24 +0100

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Jure Šunić on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
