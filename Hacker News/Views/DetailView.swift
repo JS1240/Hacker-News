d8afc39365135b8d67114db49e2334f3c6cc126a
Jure Sunic <juresunic@hotmail.com>
2022-06-03 12:25:27 +0100

    Finished the project. Used observer pattern,  url session, json decoding, swift base list view, networking, managing states.

---
+++ Hacker News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetialView.swift
+//  Hacker News
+//
+//  Created by Jure Šunić on 03/06/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
